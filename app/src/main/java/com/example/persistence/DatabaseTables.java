3f24ec338c8b2c4703dced7c83224050617a7902
AOkay12 <oscarkruit@live.se>
2024-05-06 14:02:47 +0200

    Database tables for chewinggum

--- app/src/main/java/com/example/persistence/DatabaseTables.java
+++ app/src/main/java/com/example/persistence/DatabaseTables.java
@@ -1,4 +1,23 @@
 package com.example.persistence;
 
-public class DatabaseTables {
+class DatabaseTables {
+
+    static class ChewingGum {
+
+        static final String TABLE_NAME = "chewingGums";
+        static final String COLUMN_NAME_ID = "id";
+        static final String COLUMN_NAME_TASTE = "taste";
+        static final String COLUMN_NAME_CHEWINESS = "chewiness";
+
+    }
+
+    static final String SQL_CREATE_TABLE_CHEWINGUM =
+            "CREATE TABLE " + ChewingGum.TABLE_NAME + " (" +
+                    ChewingGum.COLUMN_NAME_ID + " INTEGER PRIMARY KEY," +
+                    ChewingGum.COLUMN_NAME_TASTE + " TEXT," +
+                    ChewingGum.COLUMN_NAME_CHEWINESS + " INT)";
+
+    static final String SQL_DELETE_TABLE_CHEWINGGUM =
+            "DROP TABLE IF EXISTS " + ChewingGum.TABLE_NAME;
+
 }
