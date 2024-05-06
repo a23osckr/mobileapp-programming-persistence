6d0e6f4119463b0f9ab0022a2538ceb88fdc20bb
AOkay12 <oscarkruit@live.se>
2024-05-06 14:21:20 +0200

    Created instance of DatabaseHelper

--- app/src/main/java/com/example/persistence/MainActivity.java
+++ app/src/main/java/com/example/persistence/MainActivity.java
@@ -2,21 +2,24 @@ package com.example.persistence;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button readButton;
     private Button writeButton;
+    private SQLiteDatabase database;
+    private DatabaseHelper databaseHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // Create
+        databaseHelper = new DatabaseHelper(this);
+        database = databaseHelper.getWritableDatabase();
 
         readButton = findViewById(R.id.readButton);
         readButton.setOnClickListener(this);
