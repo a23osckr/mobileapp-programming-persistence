1901275d37e7f2104e9ad1d8f668696f43172792
AOkay12 <oscarkruit@live.se>
2024-05-06 14:20:13 +0200

    Added listeners for read & write buttons

--- app/src/main/java/com/example/persistence/MainActivity.java
+++ app/src/main/java/com/example/persistence/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.persistence;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button readButton;
+    private Button writeButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Create
+
+        readButton = findViewById(R.id.readButton);
+        readButton.setOnClickListener(this);
+
+        writeButton = findViewById(R.id.writeButton);
+        writeButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        switch (view.getId()) {
+            case R.id.readButton:
+                // Do something
+            case R.id.writeButton:
+                // DO something
+        }
     }
 }
+
