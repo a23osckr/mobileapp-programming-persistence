3fcde99244aa7fbd65f26095a1669e606b8f9cae
AOkay12 <oscarkruit@live.se>
2024-05-07 14:23:43 +0200

    Added color attribute

--- app/src/main/java/com/example/persistence/DatabaseTables.java
+++ app/src/main/java/com/example/persistence/DatabaseTables.java
@@ -8,14 +8,16 @@ class DatabaseTables {
         static final String COLUMN_NAME_ID = "id";
         static final String COLUMN_NAME_TASTE = "taste";
         static final String COLUMN_NAME_CHEWINESS = "chewiness";
+        static final String COLUMN_NAME_COLOR = "color";
 
     }
 
-    static final String SQL_CREATE_TABLE_CHEWINGUM =
+    static final String SQL_CREATE_TABLE_CHEWINGGUM =
             "CREATE TABLE " + ChewingGum.TABLE_NAME + " (" +
                     ChewingGum.COLUMN_NAME_ID + " INTEGER PRIMARY KEY," +
                     ChewingGum.COLUMN_NAME_TASTE + " TEXT," +
-                    ChewingGum.COLUMN_NAME_CHEWINESS + " INT)";
+                    ChewingGum.COLUMN_NAME_CHEWINESS + " INT," +
+                    ChewingGum.COLUMN_NAME_COLOR + " TEXT)";
 
     static final String SQL_DELETE_TABLE_CHEWINGGUM =
             "DROP TABLE IF EXISTS " + ChewingGum.TABLE_NAME;
